b150707df304cfc85758f2571f4c4e1f5dc2d8b1
VivekBansalBoxinall <vivekbansal@boxinallsoftech.com>
2024-06-06 10:33:50 +0530

    Code chnage

--- Saas/routes/seatRoutes.js
+++ Saas/routes/seatRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { loadSeatsFromFile } from "../controllers/seatControllers.js";
+import {
+  loadSeatsFromFile,
+  loadCoordinatesFromFile,
+  loadCoordinatesFromFileForAllSeats,
+} from "../controllers/seatControllers.js";
 import { verifyWithId } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/load", loadSeatsFromFile);
+router.patch("/coordinates/:name", loadCoordinatesFromFile);
+router.get("/load/coordinates", loadCoordinatesFromFileForAllSeats);
 
 export default router;
